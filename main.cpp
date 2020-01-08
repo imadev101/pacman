1f05599a0dfc4fa8d7fe20bd53f881b7db346209
imadev101 <imadev101@protonmail.com>
2020-01-08 06:51:17 +0100

    basic game logic

--- main.cpp
+++ main.cpp
@@ -5,5 +5,22 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
+
+    // initiate game
+    cout << "Welcome to TicTacToe Game" << endl;
+
+    // while game not over
+
+        // handle events
+        cout << "Computer or player turn" << endl;
+
+        // update
+        cout << "Checking victory condition" << endl;
+
+        // render
+
+    // game over
+    cout << "Game over condition met (board is full or someone won)" << endl;
+
     return 0;
 }
