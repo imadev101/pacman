7ff99ce9bcdeb79d3183cb0c20ba20e590301312
imadev101 <imadev101@protonmail.com>
2020-01-11 16:13:40 +0100

    SDL2 implementation and basic test code

--- main.cpp
+++ main.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
 #include "Game.h"
+#include "SDL.h"
 
 using namespace std;
 
-Game *game;
+Game *game = NULL;
 
-int main()
+// screen resolutions
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int arg, char* args[])
 {
-    // initiate game
+    // the window and its content
+    SDL_Window *window = NULL;
+    SDL_Surface *surface = NULL;
+
+    // initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO)<0)
+    {
+        cout << "SDL Error :" << SDL_GetError() << endl;
+        return 0;
+    }
+
+    window = SDL_CreateWindow("TicTacToe", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    if (window == NULL)
+    {
+        cout << "SDL Error :" << SDL_GetError() << endl;
+        return 0;
+    }
+
+    // get the window surface pointer
+    surface = SDL_GetWindowSurface( window );
+
+    // fill it
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xAA, 0x00));
+
+    // update
+    SDL_UpdateWindowSurface(window);
+
+    // initialize game
     cout << "Welcome to TicTacToe Game" << endl;
     game = new Game();
     game->init();
@@ -19,6 +52,12 @@ int main()
     }
 
     // game over
+
+    // free memory
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    delete game;
 
     return 0;
 }
